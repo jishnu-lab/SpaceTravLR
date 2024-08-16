c04aa08aa957bb87f2906f1d2e0a4eeaac5dcd28
Koushul Ramjattun <koush@M3-Pro.local>
2024-08-15 20:56:34 -0400

    initial version of SpaceOracle class

---
+++ src/spaceoracle/oracles.py
@@ -0,0 +1,94 @@
+from abc import ABC, abstractmethod
+import numpy as np
+import sys
+import gc
+from pympler import asizeof
+
+from .models.spatial_map import xyc2spatial
+from .models.estimators import ViTEstimatorV2
+
+class Oracle(ABC):
+    
+    def __init__(self, adata):
+        self.adata = adata.copy()
+        
+    
+    
+
+class SpaceOracle(Oracle):
+
+    def __init__(self, adata, anot='rctd_cluster', spatial_dim=64):
+        super().__init__(adata)
+        self.anot = anot
+        self.spatial_dim = spatial_dim
+        self.imbue_adata_with_space(
+            self.adata, 
+            anot=self.anot,
+            spatial_dim=self.spatial_dim,
+            in_place=True
+        )
+
+        for i in ['Cd74']:
+            estimator = ViTEstimatorV2(self.adata, target_gene=i)
+
+
+
+            print(asizeof.asizeof(estimator)/(1024*1024))
+
+
+
+            estimator.fit(
+                annot='rctd_cluster', 
+                max_epochs=2, 
+                learning_rate=3e-4, 
+                spatial_dim=self.spatial_dim,
+                batch_size=32,
+                init_betas='co',
+                mode='train_test',
+                rotate_maps=True,
+                regularize=False,
+                n_patches=16, 
+                n_heads=4, 
+                n_blocks=3, 
+                hidden_d=16
+            )
+
+            estimator.adata = None
+            estimator.grn = None
+            
+            gc.collect()
+
+            print(asizeof.asizeof(estimator)/(1024*1024))
+
+            print(asizeof.asizeof(estimator.model)/(1024*1024))
+
+
+
+
+
+
+
+    
+
+    @staticmethod
+    def imbue_adata_with_space(adata, anot='rctd_cluster', spatial_dim=64, in_place=False):
+        clusters = np.array(adata.obs[anot])
+        xy = np.array(adata.obsm['spatial'])
+
+        sp_maps = xyc2spatial(
+            xy[:, 0], 
+            xy[:, 1], 
+            clusters,
+            spatial_dim, spatial_dim, 
+            disable_tqdm=False
+        ).astype(np.float32)
+
+        if in_place:
+            adata.obsm['spatial_maps'] = sp_maps
+            return
+
+        return sp_maps
+    
+
+    
+
