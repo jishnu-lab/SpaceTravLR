eabf2fd89460f5d285d20a876c56f995b06f64bc
Koushul Ramjattun <koush@M3-Pro.local>
2024-08-19 14:39:22 -0400

    added regulator check

--- tests/test_vit.py
+++ tests/test_vit.py
@@ -5,6 +5,7 @@ import anndata as ad
 import sys
 import os
 import pandas as pd
+import scanpy as sc
 from torch.utils.data import DataLoader
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
@@ -65,3 +66,52 @@ def test_vit_attention_weights():
     assert len(att_weights) == n_blocks
 
 
+def test_vit_with_real_data():
+    adata_train = ad.read_h5ad('./data/slideseq/day3_1.h5ad')
+    adata_test = ad.read_h5ad('./data/slideseq/day3_2.h5ad')
+
+    n_top_genes = 4000
+    min_cells = 10
+    min_counts = 350
+
+    adata_train.var_names_make_unique()
+    adata_train.var["mt"] = adata_train.var_names.str.startswith("mt-")
+    sc.pp.calculate_qc_metrics(adata_train, qc_vars=["mt"], inplace=True)
+    sc.pp.filter_cells(adata_train, min_counts=min_counts)
+    adata_train = adata_train[adata_train.obs["pct_counts_mt"] < 20].copy()
+    adata_train = adata_train[:, ~adata_train.var["mt"]]
+    sc.pp.filter_genes(adata_train, min_cells=min_cells)
+
+    adata_train.layers["raw_count"] = adata_train.X
+
+    sc.pp.normalize_total(adata_train, inplace=True)
+    sc.pp.log1p(adata_train)
+    sc.pp.highly_variable_genes(
+        adata_train, flavor="seurat", n_top_genes=n_top_genes)
+
+    adata_train = adata_train[:, adata_train.var.highly_variable]
+    
+    
+    adata_test.var_names_make_unique()
+    adata_test.var["mt"] = adata_test.var_names.str.startswith("mt-")
+    sc.pp.calculate_qc_metrics(adata_test, qc_vars=["mt"], inplace=True)
+    sc.pp.filter_cells(adata_test, min_counts=min_counts)
+    adata_test = adata_test[adata_test.obs["pct_counts_mt"] < 20].copy()
+    adata_test = adata_test[:, ~adata_test.var["mt"]]
+    sc.pp.filter_genes(adata_test, min_cells=min_cells)
+
+    adata_test.layers["raw_count"] = adata_test.X
+
+    sc.pp.normalize_total(adata_test, inplace=True)
+    sc.pp.log1p(adata_test)
+    sc.pp.highly_variable_genes(
+        adata_test, flavor="seurat", n_top_genes=n_top_genes)
+
+    adata_test = adata_test[:, adata_test.var.highly_variable]
+    
+
+    adata_train = adata_train[:, adata_train.var_names.isin(np.intersect1d(adata_train.var_names, adata_test.var_names))]
+    adata_test = adata_test[:, adata_test.var_names.isin(np.intersect1d(adata_train.var_names, adata_test.var_names))]
+
+    estimator = ViTEstimatorV2(adata_train, target_gene='Cd74')
+    assert len(estimator.regulators) == 15
