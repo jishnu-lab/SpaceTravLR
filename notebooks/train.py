6565ba3ed79a45f2bfbdba0f969deb26f4405b48
Koushul Ramjattun <kor11@pitt.edu>
2024-09-02 12:26:00 -0400

    better adata

--- notebooks/train.py
+++ notebooks/train.py
@@ -4,14 +4,9 @@ import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 import scanpy as sc
 import spaceoracle
-from spaceoracle.models.estimators import ViTEstimatorV2, device
-# from utils import adata_train, adata_test
-from spaceoracle import SpaceOracle
 
 adata_train = sc.read_h5ad(
     '/ihome/ylee/kor11/space/SpaceOracle/notebooks/cache/adata_train.h5ad')
-SpaceOracle.imbue_adata_with_space(
-    adata_train, annot='rctd_cluster', spatial_dim=64, in_place=True)
 
 so = spaceoracle.SpaceOracle(
     adata=adata_train,
