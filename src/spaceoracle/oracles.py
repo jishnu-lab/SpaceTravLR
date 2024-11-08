f25e181c8b9c746db4d9b8555ef3549237c666a8
Ally <alw399@pitt.edu>
2024-11-08 16:15:11 -0500

    fix _get_wbetas_dict in case of no ligands

--- src/spaceoracle/oracles.py
+++ src/spaceoracle/oracles.py
@@ -450,7 +450,8 @@ class SpaceOracle(Oracle):
                 lig_df=gex_df[list(self.ligands)],
                 radius=self.radius
             )
-
+        else:
+            weighted_ligands = None
 
         for gene, betaoutput in tqdm(betas_dict.items(), total=len(betas_dict), desc='Ligand interactions'):
             betas_df = self._combine_gene_wbetas(gene, weighted_ligands, gex_df, betaoutput)
