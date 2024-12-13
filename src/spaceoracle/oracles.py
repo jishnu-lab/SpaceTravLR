b433420b043b0413fcc1c61a780953d0695deeca
Koushul Ramjattun <kor11@pitt.edu>
2024-12-13 11:31:26 -0500

    updated adata processing

--- src/spaceoracle/oracles.py
+++ src/spaceoracle/oracles.py
@@ -369,7 +369,7 @@ class SpaceTravLR(BaseTravLR):
 
             else:
                 gene_bar.count = len(self.queue.all_genes) - len(self.queue.remaining_genes)
-                gene_bar.desc = f'{self.queue.num_orphans} orphans'
+                gene_bar.desc = f'🕵️️ {self.queue.agents} agents'
                 gene_bar.refresh()
 
                 if os.path.exists(f'{self.queue.model_dir}/{gene}.lock'):
