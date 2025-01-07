5fee4595fb6a813d283787d63b6ed561605ccacd
Koushul Ramjattun <kor11@pitt.edu>
2025-01-07 16:54:38 -0500

    cleanup on network

--- src/spaceoracle/tools/network.py
+++ src/spaceoracle/tools/network.py
@@ -24,6 +24,12 @@ def expand_paired_interactions(df):
     
     return df
 
+def encode_labels(labels):
+    unique_labels = sorted(list(set(labels)))
+    return {i: label for i, label in enumerate(unique_labels)}
+
+
+
 class GeneRegulatoryNetwork:
     def __init__(self, organism='mouse'):
         if organism == 'mouse':
@@ -93,6 +99,49 @@ class CellOracleLinks:
             train_genes.extend(genes)
         
         return np.unique(train_genes)
+
+
+class RegulatoryFactory(CellOracleLinks):
+    def __init__(self, colink_path, organism='mouse', annot='cell_type_int'):
+        self.colink_path = colink_path
+        self.organism = organism
+        self.annot = annot
+
+        with open(self.colink_path, 'rb') as f:
+            self.links = pickle.load(f)
+
+        self.cluster_labels = encode_labels(self.links.keys())
+
+ 
+    def get_cluster_regulators(self, adata, target_gene, alpha=0.05):
+        adata_clusters = np.unique(adata.obs[self.annot])
+        regulator_dict = {}
+        all_regulators = set()
+
+        for label in adata_clusters:
+            grn_df = self.links[label]
+
+            grn_df = grn_df[(grn_df.target == target_gene) & (grn_df.p <= alpha)]
+            tfs = list(grn_df.source)
+            
+            regulator_dict[label] = tfs
+            all_regulators.update(tfs)
+
+        all_regulators = all_regulators & set(adata.to_df().columns) # only use genes also in adata
+        all_regulators = sorted(list(all_regulators))
+        regulator_masks = {}
+
+        for label, tfs in regulator_dict.items():
+            indices = [all_regulators.index(tf)+1 for tf in tfs if tf in all_regulators]
+            
+            mask = torch.zeros(len(all_regulators) + 1)     # prepend 1 for beta0
+            mask[[0] + indices] = 1 
+            regulator_masks[label] = mask
+
+        self.regulator_dict = regulator_masks
+
+        return all_regulators
+
 
         
 class SurveyRegulatoryNetwork(CellOracleLinks):
