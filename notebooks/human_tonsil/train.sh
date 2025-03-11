a0f46aad41330502fffc3a0a1c7924c054e7c079
Ally <alw399@pitt.edu>
2025-03-11 14:06:25 -0400

    kill old locks

--- notebooks/human_tonsil/train.sh
+++ notebooks/human_tonsil/train.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-#SBATCH --partition=l40s
+#SBATCH --partition=preempt
 #SBATCH --job-name=SpaceTravLR
-#SBATCH --output=/dev/null
-#SBATCH --error=/dev/null
+#SBATCH --output=train.txt
+##SBATCH --error=/dev/null
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --cluster=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --time=0-8:00:00
+#SBATCH --time=0-2:00:00
 
-mamba activate SpaceOracle
+# mamba activate SpaceOracle
+# conda init 
+conda activate bee
 python train.py
