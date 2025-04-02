48e35ea8da8a7a329db2dd54ef6c26471dea129c
Ally <alw399@pitt.edu>
2025-04-01 23:39:55 -0400

    move to local

--- src/spaceoracle/visionary.py
+++ src/spaceoracle/visionary.py
@@ -3,8 +3,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import anndata as ad
-from sklearn.metrics.pairwise import cosine_similarity
 import json
+
+from tqdm import tqdm
+
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.stats import spearmanr
+import networkx as nx 
+from networkx.algorithms import bipartite
 
 from .oracles import SpaceTravLR
 from .beta import BetaFrame
@@ -15,8 +21,9 @@ from .models.parallel_estimators import get_filtered_df, create_spatial_features
 class Visionary(GeneFactory):
     def __init__(self, ref_adata, test_adata, ref_json_path, 
                  ref_embed=None, test_embed=None, subsample=None,
-                 matching_annot=None, matching_method='banksy',
-                 prematching=None):
+                 matching_annot=None, matching_method='cnn',
+                 markers=None, prematching=None, 
+                 layer='normalized_count'):
 
         with open(ref_json_path, 'r') as f:
             params = json.load(f)
@@ -27,11 +34,19 @@ class Visionary(GeneFactory):
                          radius=params['radius'], 
                          contact_distance=params['contact_distance'])
 
+        self.layer = layer 
+        self.markers = markers
         self.ref_adata = ref_adata
-        self.load_embeds(ref_embed, test_embed, method=matching_method)
-
         self.matching_annot = self.annot if matching_annot is None else matching_annot
+
+        # make annot (cell_type_int) match for ref and test adata
+        ct_int_mapping = {
+            matching_label: i for matching_label, i in self.ref_adata.obs[[self.matching_annot, self.annot]].value_counts().index}
+        self.adata.obs[self.annot] = self.adata.obs[self.matching_annot].map(ct_int_mapping)
+
         if prematching is None:
+            self.load_embeds(ref_embed, test_embed, method=matching_method)
+
             self.match_embeddings()
         else:
             self.matching = prematching
@@ -83,26 +98,49 @@ class Visionary(GeneFactory):
 
         return banksy_matrix.to_df()
         
-    
-    def compute_covet_embed(self, adata):
+    @staticmethod
+    def compute_covet_embed(ref_adata, test_adata, k=15):
         import scenvi 
 
-        # COVET uses the log normalized layer
-        st_data = ad.AnnData(
-            adata.layers['normalized_count'],
-            var=adata.var,
-            obsm=adata.obsm
-        )
-        st_data.layers['log'] = st_data.X
-        
-        # We want the SQRT to compare embeddings
-        _, st_data.obsm['COVET_SQRT'], _ = scenvi.compute_covet(st_data)
-        del st_data.layers['log']
+        test_coords = test_adata.obsm['spatial'].copy()
+        test_coords[:, 0] += ref_adata.obsm['spatial'][:, 0].max() + 1
+        test_coords[:, 1] += ref_adata.obsm['spatial'][:, 1].max() + 1
 
-        return st_data.obsm['COVET_SQRT']
+        adata = ad.AnnData(
+            np.concatenate([ref_adata.layers['normalized_count'], test_adata.layers['normalized_count']], axis=0),
+            obs=range(ref_adata.shape[0] + test_adata.shape[0]),
+            var=ref_adata.var,
+            obsm={
+                'spatial': np.concatenate([ref_adata.obsm['spatial'], test_coords], axis=0)
+            }
+        )
+        adata.layers['log'] = adata.X
+
+        # We want the SQRT to compare embeddings
+        _, adata.obsm['COVET_SQRT'], _ = scenvi.compute_covet(adata, k=k)
+
+        n_refs = ref_adata.shape[0]
+        ref_embed = adata.obsm['COVET_SQRT'][:n_refs]
+        test_embed = adata.obsm['COVET_SQRT'][n_refs:]
+
+        ref_embed = pd.DataFrame(
+            ref_embed.reshape(n_refs, -1),
+            index=ref_adata.obs.index,
+            columns=[f'COVET_{i}' for i in range(ref_embed.shape[1]**2)]
+        )
+        test_embed = pd.DataFrame(
+            test_embed.reshape(test_adata.shape[0], -1),
+            index=test_adata.obs.index,
+            columns=[f'COVET_{i}' for i in range(test_embed.shape[1]**2)]
+        )
+
+
+        return ref_embed, test_embed
 
     def compute_cnn_embed(self, adata):
-        sp_maps = SpaceTravLR.imbue_adata_with_space(adata, annot=self.annot)
+        
+        # sp_maps = SpaceTravLR.imbue_adata_with_space(adata, annot=self.annot)
+        
         spatial_features = create_spatial_features(
             adata.obsm['spatial'][:, 0], 
             adata.obsm['spatial'][:, 1], 
@@ -110,8 +148,8 @@ class Visionary(GeneFactory):
             adata.obs.index,
             radius=self.radius
         )
-        raise NotImplementedError('CNN embeddings are not yet implemented')
-        return None
+
+        return spatial_features
 
     def load_embeds(self, ref_embed, test_embed, method='covet'):
 
@@ -132,24 +170,15 @@ class Visionary(GeneFactory):
                 ref_embed = self.compute_cnn_embed(self.ref_adata)
             if test_embed is None:
                 test_embed = self.compute_cnn_embed(self.adata)
+
+            ref_embed = ref_embed[test_embed.columns]
        
         elif method == 'covet':
 
-            if ref_embed is None:
-                ref_embed = self.compute_covet_embed(self.ref_adata)
-            if test_embed is None:
-                test_embed = self.compute_covet_embed(self.adata)
-        
-            ref_embed = pd.DataFrame(
-                ref_embed.reshape(ref_embed.shape[0], -1),
-                index=self.ref_adata.obs.index,
-                columns=[f'COVET_{i}' for i in range(ref_embed.shape[1]**2)]
-            )
-            test_embed = pd.DataFrame(
-                test_embed.reshape(test_embed.shape[0], -1),
-                index=self.adata.obs.index,
-                columns=[f'COVET_{i}' for i in range(test_embed.shape[1]**2)]
-            )
+            if ref_embed is None or test_embed is None:
+                ref_embed, test_embed = self.compute_covet_embed(
+                    self.ref_adata, self.adata
+                )
         
         else:
             raise ValueError('Please choose a valid method: banksy, cnn, or covet')
@@ -158,7 +187,7 @@ class Visionary(GeneFactory):
         self.test_embed = test_embed
 
     @staticmethod
-    def _match_cells_cosine(test_embed, ref_embed):
+    def match_cells_cosine(test_embed, ref_embed):
 
         cos_sim_matrix = cosine_similarity(test_embed, ref_embed)
         closest_indices = np.argmax(cos_sim_matrix, axis=1)
@@ -168,7 +197,16 @@ class Visionary(GeneFactory):
         return matched
     
     @staticmethod
-    def _match_cells_difference(test_embed, ref_embed):
+    def match_cells_spearman(test_embed, ref_embed):
+        spearman_matrix, _ = spearmanr(test_embed, ref_embed, axis=1)
+        closest_indices = np.argmax(spearman_matrix, axis=1)
+        matched_refs = ref_embed.iloc[closest_indices]
+        matched = matched_refs.index
+        return matched
+
+    
+    @staticmethod
+    def match_cells_difference(test_embed, ref_embed):
 
         diff_matrix = np.abs(
             test_embed.values.astype(np.float16)[:, None] - ref_embed.values.astype(np.float16))
@@ -181,11 +219,47 @@ class Visionary(GeneFactory):
 
         return matched
     
+
+    @staticmethod
+    def match_cells_bipartite(test_embed, ref_embed):
+        
+        matching = pd.DataFrame(index=test_embed.index, columns=['reference_cell'])
+        
+        spatial_corr = np.corrcoef(test_embed.values, ref_embed.values)
+        cost = 1 - spatial_corr
+
+        assert not np.any(np.isnan(cost)), f'NaN values found in the cost matrix'
+
+        source_nodes = list(test_embed.index)
+        target_nodes = list(ref_embed.index)
+
+        B = nx.Graph()
+        B.add_nodes_from(source_nodes, bipartite=0)
+        B.add_nodes_from(target_nodes, bipartite=1)
+
+        df = pd.DataFrame(cost[:len(test_embed), len(test_embed):], index=source_nodes, columns=target_nodes)
+        edges = df.stack().reset_index(name='weight')
+        edges_list = list(zip(edges['level_0'], edges['level_1'], edges['weight']))
+        B.add_weighted_edges_from(edges_list)
+
+        m = bipartite.minimum_weight_full_matching(B, top_nodes=test_embed.index, weight='weight')
+        matching = pd.DataFrame.from_dict(m, orient='index', columns=['reference_cell'])
+        matching = matching.loc[test_embed.index, :]
+
+        return matching['reference_cell']
+
+
     def match_cells(self, test_embed, ref_embed):
+        '''Return an index of the reference cells matching the test cell order'''
         if self.method == 'covet':
-            return self._match_cells_difference(test_embed, ref_embed)
+            return self.match_cells_difference(test_embed, ref_embed)
         elif self.method == 'banksy':
-            return self._match_cells_cosine(test_embed, ref_embed)
+            # return self.match_cells_spearman(test_embed, ref_embed)
+            # return self.match_cells_cosine(test_embed, ref_embed)
+            return self.match_cells_bipartite(test_embed, ref_embed)
+        elif self.method == 'cnn':
+            # return self.match_cells_spearman(test_embed, ref_embed)
+            return self.match_cells_bipartite(test_embed, ref_embed)
         else:
             raise ValueError('Please choose a valid method')
 
@@ -199,9 +273,11 @@ class Visionary(GeneFactory):
         test_cells = self.adata.obs.index
 
         matched = pd.DataFrame(index=test_cells , columns=['reference_cell'])
-        for ct, ct_mask in test_cts.items():
+        for ct, ct_mask in tqdm(test_cts.items(), total=len(test_cts), desc=f'Matching cells by {matching_annot}'):
+            
             matched.loc[test_cells[ct_mask], 'reference_cell'] = self.match_cells(
                 self.test_embed.loc[ct_mask], self.ref_embed[ref_cts[ct]]).values
+            
             self.adata.obs.loc[ct_mask, 'reference_cell'] = matched.loc[test_cells[ct_mask], 'reference_cell'].values
         
         self.matching = matched
