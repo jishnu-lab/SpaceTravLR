288bf83f9ef52d9c6fed4b4040c48a4af572eaca
Ally <alw399@pitt.edu>
2025-05-04 14:11:22 -0400

    cosmx

--- notebooks/mouse_kidney_survey/train.sh
+++ notebooks/mouse_kidney_survey/train.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --partition=l40s
+#SBATCH --partition=htc
 #SBATCH --job-name=SpaceTravLR
-#SBATCH --output=/dev/null
-#SBATCH --error=/dev/null
+#SBATCH --output=train_COVET.txt
+##SBATCH --error=/dev/null
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --cluster=gpu
-#SBATCH --gres=gpu:1
+##SBATCH --cluster=gpu
+##SBATCH --gres=gpu:1
 #SBATCH --time=0-8:00:00
 
 mamba activate SpaceOracle
-python train.py
+python train_COVET.py
