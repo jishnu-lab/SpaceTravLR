d5179f20dea23c06500f90c4d4f905e94800be4c
Ally <alw399@pitt.edu>
2025-05-07 15:07:51 -0400

    lymph preprocessing and covet

--- notebooks/mouse_kidney_survey/train.sh
+++ notebooks/mouse_kidney_survey/train.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-#SBATCH --partition=htc
+#SBATCH --partition=l40s
 #SBATCH --job-name=SpaceTravLR
 #SBATCH --output=train_COVET.txt
 ##SBATCH --error=/dev/null
 #SBATCH --nodes=1
+#SBATCH --mem=50G
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-##SBATCH --cluster=gpu
-##SBATCH --gres=gpu:1
+#SBATCH --cluster=gpu
+#SBATCH --gres=gpu:1
 #SBATCH --time=0-8:00:00
 
-mamba activate SpaceOracle
+# mamba activate SpaceOracle
+conda init
+source ~/.bashrc
+conda activate bee
+echo $CONDA_DEFAULT_ENV
 python train_COVET.py
