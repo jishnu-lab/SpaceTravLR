5ead5754627f56ac9afd78815c087bfc9a14b5dc
alw399 <alw399@github.com>
2025-06-16 13:42:19 -0400

    add benchmark notebooks

---
+++ notebooks/benchmark/scGPT-tonsil/analysis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+##SBATCH --partition=preempt
+#SBATCH --cluster=htc
+#SBATCH --mem=180G
+#SBATCH --job-name=scGPT_tonsil
+#SBATCH --output=analysis.txt
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=12:00:00
+
+conda init
+source ~/.bashrc
+conda activate /ix3/djishnu/alw399/envs/sheep
+echo $CONDA_DEFAULT_ENV
+
+python analysis.py
