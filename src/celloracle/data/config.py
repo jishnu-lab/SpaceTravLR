0b53cb44cec077ff3bd22069ba02f1537a1e46c1
Koushul Ramjattun <kor11@pitt.edu>
2025-10-05 09:54:35 -0400

    removed celloracle stuff

--- src/celloracle/data/config.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from pathlib import Path
-import os
-
-CELLORACLE_DATA_DIR = os.path.join(str(Path.home()), "celloracle_data")
-WEB_PAR_DIR = "https://raw.githubusercontent.com/morris-lab/CellOracle/master/celloracle/data"
